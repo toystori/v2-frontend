d4ed75499062e02d96689c6d5e8f2e999c85b468
Pradip Caulagi <caulagi@gmail.com>
2020-08-02 22:12:03 +0200

    Upgrade react-native to 0.62.2
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 /**
  * Do not modify this file - it is a proxy to your `App.re` file
  * located in the `src/` folder.
+ *
+ * @format
+ * @flow strict-local
  */
 export { app as default } from "./src/App.bs.js";
